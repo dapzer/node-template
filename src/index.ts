5364a99a921ab3af4e836622327a5bb012548d07
dapzer <danilavoronkov2002@gmail.com>
2025-11-04 18:15:31 +0300

    feat: allow any depth of logs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import process from "node:process"
+import * as util from "node:util"
 import { config } from "@/shared/constants"
 import { logger } from "@/shared/lib/logger"
 import { getUtcNow } from "@/shared/utils/getUtcNow"
+
+// * Disable limit for console log object
+util.inspect.defaultOptions.depth = null
+util.inspect.defaultOptions.maxArrayLength = null
 
 async function bootstrap() {
   logger.info(`App successfully started at ${getUtcNow()}, environment ${config.NODE_ENV}`)
